3782efc4ceec06822f268693902814b452951d19
kamankwah77 <147664380+kamankwah77@users.noreply.github.com>
2025-12-08 23:26:49 +0000

    trying deployment

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ app.use(cors({
         const allowedOrigins = [
             'https://campus-food-frontend.onrender.com',
             'http://localhost:3000',
-            'http://localhost:5000',
+            'http://localhost:10000',
             'http://localhost:10000',  
             'http://127.0.0.1:5500',
             'http://localhost:5500',
